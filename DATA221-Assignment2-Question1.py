c817c06f4cb7b2e0af6313a5082f3075206202e5
Abigail Spring <abigail.spring@ucalgary.ca>
2026-02-10 19:49:28 -0700

    Added more specific names to Question 1

--- DATA221-Assignment2-Question1.py
+++ DATA221-Assignment2-Question1.py
@@ -2,15 +2,15 @@ import string
 from collections import Counter
 
 with open("sample-file.txt", "r", encoding="utf-8") as file:
-    text = file.read()
-tokens = text.split()
-clean_tokens = []
-for token in tokens:
+    text_in_file = file.read()
+tokens_in_chosen_file = text_in_file.split()
+clean_tokens_in_file = []
+for token in tokens_in_chosen_file:
     token = token.lower()
     token = token.strip(string.punctuation)
     if sum(char.isalpha() for char in token) >= 2:
-        clean_tokens.append(token)
-word_counts = Counter(clean_tokens)
+        clean_tokens_in_file.append(token)
+word_counts = Counter(clean_tokens_in_file)
 top_10 = word_counts.most_common(10)
 for word, count in top_10:
     print(f"{word} -> {count}")
